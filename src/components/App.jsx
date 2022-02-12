2cedea3a249d07de4d424b3441dffb730c071995
unknown <surajverma8787@gmail.com>
2022-02-12 17:21:00 +0530

    Keeper App Part 1

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Heading from "./Heading.jsx";
+import Footer from "./Footer.jsx";
+import Note from "./Note.jsx";
+function App() {
+    return <div>
+        <Heading />
+        <Note />
+        <Footer />
+    </div>
+}
+export default App;
